f6a458b8ce03a4d169eefdc014208880db7f94c3
Giorgi Kutubidze <ffs@gmail.com>
2024-04-12 12:15:48 +0400

    client

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,13 +1,29 @@
-'use client'
-import React from 'react'
+'use client '
+import React, { useEffect, useState } from 'react'
 import { HeaderDesktop } from './HeaderDesktop';
 import HeaderMobile from './HeaderMobile';
 
 const Header = () => {
+    const [isMobile, setIsMobile] = useState(false);
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 1024);
+        };
 
-    return (
-        window.innerWidth<1024 ? <HeaderMobile/> : <HeaderDesktop/> 
-    )
+        // Initial check
+        handleResize();
+
+        // Event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup function to remove event listener
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return isMobile ? <HeaderMobile /> : <HeaderDesktop />;
+
 }
 
 export default Header
