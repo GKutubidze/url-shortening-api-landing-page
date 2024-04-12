e85e2aed85c8e7d1970eb6c831c0fca785e73926
Giorgi Kutubidze <ffs@gmail.com>
2024-04-12 12:11:31 +0400

    fix width

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { HeaderDesktop } from './HeaderDesktop';
 import HeaderMobile from './HeaderMobile';
-type Props={
-    width:number;
-}
-const Header = ({width}:Props) => {
-    
+
+const Header = () => {
+
     return (
-        width<1024 ? <HeaderMobile/> : <HeaderDesktop/> 
+        window.innerWidth<1024 ? <HeaderMobile/> : <HeaderDesktop/> 
     )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,25 @@ import Header from "./components/Header/Header";
 import useWindowWidth from "./hooks/useWindowWidth";
 
 export default function Home() {
-  const width=useWindowWidth();
-  const [url,setUrl]=useState<string>(" ");
+  const width = useWindowWidth();
+  const [url, setUrl] = useState<string>(" ");
   const [tempUrl, setTempUrl] = useState<string>("");
 
   console.log(url);
-   return (
+  return (
     <main className={styles.main}>
-      <Header width={width} />
+      <Header />
       <ContentComponent width={width} />
       <div className={styles.ShortenCompContainer}>
-      <ShortenComponent url={url} setUrl={setUrl} tempUrl={tempUrl} setTempUrl={setTempUrl} />
-
+        <ShortenComponent
+          url={url}
+          setUrl={setUrl}
+          tempUrl={tempUrl}
+          setTempUrl={setTempUrl}
+        />
       </div>
       <AdvancedStats />
       <Footer />
     </main>
   );
 }
- 
